9ae181032a3cfd4003419e6a61a212b6c3f95d46
Saketh Ramanujam <saketh.ramanujam98@hotmail.com>
2018-07-21 00:17:35 +0530

    add google analytics support

---
+++ js/gtag.js
@@ -0,0 +1,5 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-109024518-2');
+gtag('set', {'user_id': 'UA-109024518-2'});
